70fbd74c2e2b1353fd7c20dd79de6c6b89523ea8
yoo27 <75994573+yoo27@users.noreply.github.com>
2020-12-20 21:15:34 +0700

    BuatPause dan LoadScene Script
    BuatPause = untuk canvas pause , pencet 'esc' untuk pause blm gua test.
    
    LoadScene = untuk load ke scene lain, blm gw test , disitu ad  buat exit app juga

---
+++ Uflonne/Assets/Scripts/LoadSceneScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadSceneScript : MonoBehaviour
+{
+    public void GoToScene(int idScene)
+    {
+        SceneManager.LoadScene(idScene);
+    }
+
+    public void GoToScene(string nameScene)
+    {
+        SceneManager.LoadScene(nameScene);
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
